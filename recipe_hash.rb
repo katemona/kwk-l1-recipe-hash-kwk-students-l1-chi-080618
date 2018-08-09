a92f7f4bee2ba76150a3adc4b62a02270e56fa77
katemona <k.monahan@mothermcauley.org>
2018-08-09 19:32:45 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -21,18 +21,7 @@ def recipe_ingredients(perfect_10_recipe)
 end
 
 #2: Write code that returns the amount of chocolate chips from perfect_10 ingredients that is needed in the recipe.
-def amount_of_chocolate_chips(perfect_10_recipe)
-  perfect_10_recipe = {:almond_flour => "2 cups",
-      :gluten_free_whole_oats => "1 cup",
-      :kosher_salt => "1/2 tea spoon",
-      :baking_powder => "1/2 tea spoon",
-      :baking_soda => "1/4 tea spoon",
-      :xanthan_Gum => "1/2 tea spoon",
-      :slivered_almonds => "1/4 cup",
-      :mini_dark_chocolate_chips => "3/4 cup",
-      :olive_oil => "1/2 cup",
-      :agave => "1/4 cup"
-    }
+def amount_of_chocolate_chips
   return perfect_10_recipe{:mini_dark_chocolate_chips}
 end
 
